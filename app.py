5aa933610a4d3f3788cbbde92a3faf42c9ccb39b
ducminh11102012 <ducminh@mail.dangminhhoa.edu.vn>
2025-09-21 22:22:33 +0700

    Update app.py

--- app.py
+++ app.py
@@ -750,6 +750,8 @@ def send_winner_email(draw_id):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-        # Cập nhật cấu hình mail khi khởi động
         update_mail_config()
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))  # lấy port từ môi trường
+    app.run(host="0.0.0.0", port=port, debug=True)  # host=0.0.0.0 để cloud truy cập
+
